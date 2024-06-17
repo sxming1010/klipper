07b49eaabf296fde2c5e5d1a3007f5f6ceea7a57
SXming <155040130+sxming1010@users.noreply.github.com>
2024-06-17 23:48:12 +0800

    Update corexy.py

--- klippy/kinematics/corexy.py
+++ klippy/kinematics/corexy.py
@@ -1,7 +1,5 @@
 # Code for handling the kinematics of corexy robots
-#
 # Copyright (C) 2017-2021  Kevin O'Connor <kevin@koconnor.net>
-#
 # This file may be distributed under the terms of the GNU GPLv3 license.
 import logging, math
 import stepper
